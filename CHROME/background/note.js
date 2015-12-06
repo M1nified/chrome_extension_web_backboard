beaf79793055755d793e7c6047a0af93e0679e86
Minified <gora.pub@gmail.com>
2015-12-06 18:57:36 +0100

    v 0.1.1.1
    added paintPalette prototype with saving in chrome.storage.local

--- CHROME/background/note.js
+++ CHROME/background/note.js
@@ -1,12 +1,78 @@
-var color = "#000";
+function paintPalette(){
+	var printLastColors = function(){
+		$('#pwj_last_colors').empty().append(function(){
+			// console.log(last_colors)
+			var ret='';
+			for(i in last_colors){
+				ret+='<button class="pwj_last_color" data-color="'+last_colors[i]+'" style="width: 50px;height:25px; background-color:'+last_colors[i]+';"></button>'
+			}
+			return ret;
+		}.call())
+	}
+	var addLastColors = function(colors){
+		// console.log(typeof colors);
+		var addElement = function(elem){
+			if(last_colors.indexOf(elem)===-1){
+				last_colors.push(elem);
+			}
+		}
+		// console.log('add all');
+		if(typeof colors === 'object'){
+			for(i in colors){
+				addElement(colors[i]);
+			}
+		}else if(typeof colors === 'string'){
+			// console.log('2');
+			addElement(colors);
+		}
+		// console.log(last_colors);
+		if(last_colors.length>10){
+			last_colors = last_colors.slice(0,10);
+		}
+		chrome.storage.local.set({last_colors:last_colors})
+		printLastColors();
+	}
+	var last_colors = [];
+	var	color = "#000000";
+	chrome.storage.local.get(['last_colors'], function(data){
+		if(data){
+			addLastColors(data.last_colors);
+		}
+	})
+	return {
+		color : color,
+		last_colors : last_colors,
+		setColor : function setColor(color){
+			if(this.color===color){
+				return;
+			}
+			this.color = color;
+			addLastColors(this.color);
+			$('.pwj_color_picker').val(this.color);
+		},
+		addLastColors : addLastColors
+	}
+}
+//var paintPalette = {};
+
+
 var runWebNote = function(){
 	console.log("LOADED")
-	setEnv();	
+	setEnv();
+	paintPalette = new paintPalette();
 }
 var setEnv = function(){
 	$(".pwj_elem").remove();
 	if(!$(".pwj_elem")[0]){
-		$("body").append('<div class="pwj_draw_box pwj_elem"><canvas id="pwj_draw"></canvas></div><div class="pwj_elem" id="pwj_tool_box"><input type="color" class="pwj_color_picker"> <button class="pwj_button undo_canvas">&lt;-</button></div><section class="pwj_elem" id="pwj_spawner"></section>')
+		$("body").append('<div class="pwj_draw_box pwj_elem">\
+		<canvas id="pwj_draw"></canvas>\
+		</div>\
+		<div class="pwj_elem" id="pwj_tool_box">\
+		<input type="color" class="pwj_color_picker" title="Pick color">\
+		<section id="pwj_last_colors"></section>\
+		<button class="pwj_button undo_canvas" title="Undo paint">Undo</button>\
+		</div>\
+		<section class="pwj_elem" id="pwj_spawner"></section>')
 		setLogic();
 	}
 	optimizeView();
@@ -22,7 +88,7 @@ var optimizeView = function(){
 }
 var setLogic = function(){
 	$(".pwj_draw_box").on('dblclick',function(evt){
-		console.log(evt)
+		//console.log(evt)
 		spawnNote(evt);
 	})
 	$("canvas#pwj_draw").on('mousemove',function(evt){
@@ -39,7 +105,7 @@ var setLogic = function(){
 		}
 	}).on('mousedown mouseup',function(evt){
 		pathon=!pathon;
-		console.log(pathon)
+		//console.log(pathon)
 		var canvas = document.getElementById("pwj_draw")
 		var ctx = canvas.getContext("2d")
 		if(pathon){
@@ -53,8 +119,7 @@ var setLogic = function(){
 		optimizeView();
 	})
 	$(".pwj_color_picker").on('change',function(){
-		var c = $(this).val();
-		color = c;
+		paintPalette.setColor($(this).val());
 	})
 	$(".undo_canvas").on('click',function(){
 		undoCanvas();
@@ -86,9 +151,9 @@ var printPixel = function(ctx,x,y){
 	//ctx.fillRect(x, y, 1, 1);
 	//
 	//ctx.arc(x,y,4,0,2*Math.PI)
-	
+
 	ctx.lineTo(x,y);
-	ctx.strokeStyle = color;
+	ctx.strokeStyle = paintPalette.color;
 	ctx.stroke();
 	//console.log(x+" "+y)
 }
@@ -113,5 +178,20 @@ var undoCanvas = function(){
 var spawnNote = function(evt){
 	var x = evt.offsetX;
 	var y = evt.offsetY;
-	$("#pwj_spawner").append('<div class="pwj_note_box" style="top:'+y+'px;left:'+x+'px;"><textarea class="pwj_note"></textarea></div>')
+	var box = $('<div class="pwj_note_box untouchable" style="top:'+y+'px;left:'+x+'px;"><textarea class="pwj_note"></textarea></div>');
+	$("#pwj_spawner").append(box);
+	$('textarea',box).focus();
 }
+
+$(window).on('keydown',function windowOnKeydown(evt){
+	// console.log(evt);
+	if(evt.keyCode === 17 || evt.which === 17){
+		$('.untouchable').addClass('touch-me-now');
+	}
+})
+$(window).on('keyup',function windowOnKeyup(evt){
+	// console.log(evt);
+	if(evt.keyCode === 17 || evt.which === 17){
+		$('.untouchable').removeClass('touch-me-now');
+	}
+})
